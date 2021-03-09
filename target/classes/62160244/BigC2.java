5203cea9cbe9119e9f7a52302b7eccef5b2db17a
Defalt <71940630+Defalt643@users.noreply.github.com>
2021-03-09 22:22:00 +0700

    Update

--- target/classes/62160244/BigC2.java
+++ target/classes/62160244/BigC2.java
@@ -2,7 +2,24 @@ import java.net.UnknownHostException;
 import java.util.*;
 import com.mongodb.*;
 public class BigC2 {
-	public static void DisplayDocument() throws UnknownHostException {
+	public static void DisplayDocumentWithPassword() throws UnknownHostException {
+		MongoClient mongo =new MongoClient("localhost",27017);
+		DB db = mongo.getDB("bigc");
+		DBCollection table = db.getCollection("customer");
+		DBCursor cursor = table.find();							//create a variable DBCursor type from DBCursor class and assign it to a cursor that will iterate over every object.
+		int o=0;												//create a variable int type named "o" and assign it to 0
+		while(cursor.hasNext()) {								//create loop while cursor.hasNext() are return true.
+			cursor.next();										//moves the cursor ahead by one.
+			o++;												//o=o+1
+			if(cursor.hasNext()) {								//Checks if there is another object available. 
+				System.out.println(o+". Name: "+cursor.curr().get("name")+"\n   Age: "+cursor.curr().get("age")+"\n   Password: "+cursor.curr().get("password")+"\n-----------------------------------------");
+				//print sequence of the document and the information in it includes name and age and password. And print "-----------------------------------------" and Leave 1 line.
+			}else {
+				System.out.println(o+". Name: "+cursor.curr().get("name")+"\n   Age: "+cursor.curr().get("age")+"\n   Password: "+cursor.curr().get("password")+"\n\n");
+				//print sequence of the document and the information in it includes name and age and password.And leave 2 lines by using and \n\n.
+			}
+		}
+	}public static void DisplayDocument() throws UnknownHostException {
 		MongoClient mongo =new MongoClient("localhost",27017);
 		DB db = mongo.getDB("bigc");
 		DBCollection table = db.getCollection("customer");
@@ -23,16 +40,36 @@ public class BigC2 {
 	
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
+		boolean login=false;
 		try {
 			String menu;//create variable string type named "menu".
 			do {
-				System.out.println("   ********** Data Management for BigC Customer **********\n   MENU\n0. Exit");//Display menu
-				System.out.println("1. Showing All of Databases Name\n2. Showing All of Document of the Customer Collection");//Display menu
-				System.out.println("3. Adding the Person information\n4. Editing the Person information\n5. Removing the Person information");//Display menu
-				System.out.print("\nInput menu number: ");					//Display "Input menu number:"
 				MongoClient mongo =new MongoClient("localhost",27017);		//Create object named "mongo" for connect to the databases.
 				DB db = mongo.getDB("bigc");								//Create a variable DB type named "db" and assign it to the "bigc" database.
 				DBCollection table = db.getCollection("customer");			//Create a variable DBCollection type named "table" and assign it to the "customer" collection.
+				while(true) {
+					if(login==true) {
+						break;
+					}System.out.println("*** Data Management for BigC Customer ***");
+					System.out.print("******* Login ******\nUser: ");
+					String user=kb.next();
+					System.out.print("Password: ");
+					String password=kb.next();
+					BasicDBObject query = new BasicDBObject();
+					query.put("name", user);
+					query.put("password", password);
+					DBCursor cursor = table.find(query);
+					if(cursor.hasNext()) {
+						login=true;
+						break;
+					}else {
+						System.out.println("Incorrect information, please you input tried again");
+					}
+				}
+				System.out.println("   ********** Data Management for BigC Customer *************\n   MENU\n0. Exit");
+				System.out.println("1. Showing All of Databases Name\n2. Showing All of Document of the Customer Collection");//Display menu
+				System.out.println("3. Adding the Person information\n4. Editing the Person information\n5. Removing the Person information");//Display menu
+				System.out.print("\nInput menu number: ");					//Display "Input menu number:"
 				menu=kb.next(); 											//Take input from user.
 				switch(menu) {												//use switch case instead if-else statement.
 				case "0"://if menu = 0 .
@@ -58,9 +95,11 @@ public class BigC2 {
 					document.put("name", kb.next());
 					System.out.print("Age: ");
 					document.put("age", kb.nextInt());
+					System.out.print("Password: ");
+					document.put("password", kb.next());
 					table.insert(document);
 					System.out.println("****** Adding successfully ******");
-					DisplayDocument();
+					DisplayDocumentWithPassword();
 					break;
 				case "4":
 					System.out.println("\n\n************ Editing the Person information *************************");
@@ -73,15 +112,17 @@ public class BigC2 {
 						query.put("name", name);
 						DBCursor cursor = table.find(query);
 						if(cursor.hasNext()) {
-							System.out.print("**Old data:**\nName:"+cursor.next().get("name")+"\nAge:"+cursor.curr().get("age"));
+							System.out.print("**Old data:**\nName:"+cursor.next().get("name")+"\nAge:"+cursor.curr().get("age")+"\nPassword:"+cursor.curr().get("password"));
 							System.out.print("\n**New data:**\nName:");
 							newDocument.put("name", kb.next());
 							System.out.print("Age:");
 							newDocument.put("age", kb.nextInt());
+							System.out.print("Password:");
+							newDocument.put("password", kb.next());
 							updateObj.put("$set", newDocument);
 							table.update(query, updateObj);
 							System.out.println("****** Editing Data Successfully ******");
-							DisplayDocument();
+							DisplayDocumentWithPassword();
 							break;
 						}else {
 							System.out.println("\""+name+"\" Not found in database.");
