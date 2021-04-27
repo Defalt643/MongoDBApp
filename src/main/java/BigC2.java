b08db20feda6af7043417ca5a425f7e116634c0d
Khummeung Wattanasaroj <71940630+Defalt643@users.noreply.github.com>
2021-04-28 03:59:59 +0700

    Update BigC2.java

--- src/main/java/BigC2.java
+++ src/main/java/BigC2.java
@@ -7,11 +7,11 @@ public class BigC2 {
 		DB db = mongo.getDB("bigc");
 		DBCollection table = db.getCollection("customer");
 		DBCursor cursor = table.find();							//create a variable DBCursor type from DBCursor class and assign it to a cursor that will iterate over every object.
-		int o=0;												//create a variable int type named "o" and assign it to 0
-		while(cursor.hasNext()) {								//create loop while cursor.hasNext() are return true.
-			cursor.next();										//moves the cursor ahead by one.
-			o++;												//o=o+1
-			if(cursor.hasNext()) {								//Checks if there is another object available. 
+		int o=0;									//create a variable int type named "o" and assign it to 0
+		while(cursor.hasNext()) {							//create loop while cursor.hasNext() are return true.
+			cursor.next();								//moves the cursor ahead by one.
+			o++;									//o=o+1
+			if(cursor.hasNext()) {							//Checks if there is another object available. 
 				System.out.println(o+". Name: "+cursor.curr().get("name")+"\n   Age: "+cursor.curr().get("age")+"\n   Password: "+cursor.curr().get("password")+"\n-----------------------------------------");
 				//print sequence of the document and the information in it includes name and age and password. And print "-----------------------------------------" and Leave 1 line.
 			}else {
@@ -24,11 +24,11 @@ public class BigC2 {
 		DB db = mongo.getDB("bigc");
 		DBCollection table = db.getCollection("customer");
 		DBCursor cursor = table.find();							//create a variable DBCursor type from DBCursor class and assign it to a cursor that will iterate over every object.
-		int o=0;												//create a variable int type named "o" and assign it to 0
-		while(cursor.hasNext()) {								//create loop while cursor.hasNext() are return true.
-			cursor.next();										//moves the cursor ahead by one.
-			o++;												//o=o+1
-			if(cursor.hasNext()) {								//Checks if there is another object available. 
+		int o=0;									//create a variable int type named "o" and assign it to 0
+		while(cursor.hasNext()) {							//create loop while cursor.hasNext() are return true.
+			cursor.next();								//moves the cursor ahead by one.
+			o++;									//o=o+1
+			if(cursor.hasNext()) {							//Checks if there is another object available. 
 				System.out.println(o+". Name: "+cursor.curr().get("name")+"\n   Age: "+cursor.curr().get("age")+"\n-----------------------------------------");
 				//print sequence of the document and the information in it includes name and age. And print "-----------------------------------------" and Leave 1 line.
 			}else {
@@ -45,8 +45,8 @@ public class BigC2 {
 			String menu;//create variable string type named "menu".
 			do {
 				MongoClient mongo =new MongoClient("localhost",27017);		//Create object named "mongo" for connect to the databases.
-				DB db = mongo.getDB("bigc");								//Create a variable DB type named "db" and assign it to the "bigc" database.
-				DBCollection table = db.getCollection("customer");			//Create a variable DBCollection type named "table" and assign it to the "customer" collection.
+				DB db = mongo.getDB("bigc");					//Create a variable DB type named "db" and assign it to the "bigc" database.
+				DBCollection table = db.getCollection("customer");		//Create a variable DBCollection type named "table" and assign it to the "customer" collection.
 				while(true) {
 					if(login==true) {
 						break;
@@ -69,25 +69,25 @@ public class BigC2 {
 				System.out.println("   ********** Data Management for BigC Customer *************\n   MENU\n0. Exit");
 				System.out.println("1. Showing All of Databases Name\n2. Showing All of Document of the Customer Collection");//Display menu
 				System.out.println("3. Adding the Person information\n4. Editing the Person information\n5. Removing the Person information");//Display menu
-				System.out.print("\nInput menu number: ");					//Display "Input menu number:"
-				menu=kb.next(); 											//Take input from user.
-				switch(menu) {												//use switch case instead if-else statement.
+				System.out.print("\nInput menu number: ");				//Display "Input menu number:"
+				menu=kb.next(); 							//Take input from user.
+				switch(menu) {								//use switch case instead if-else statement.
 				case "0"://if menu = 0 .
 					System.out.println("\n********** Exit *************\nThank you for Data Managing\nGoodbye\n\n");//print interface.
-					break;													//break.
-				case "1":													//if menu = 1.
+					break;								//break.
+				case "1":								//if menu = 1.
 					System.out.println("\n********** Show All of Databases Name *************");//print interface.
 					List<String> dbs = mongo.getDatabaseNames();			//create List string type named "dbs" and assign it to database name from method .getDatabaseNames()
-					int i=0;												//create a variable int type named "i" and assign it to 0 for count and display databases sequence.
-					for(String DB : dbs) {									//create for loop to display database name.
-						i++;												//i=i+1
-						System.out.println(i+". "+DB);						//print databases sequence and database name.
-					}System.out.println("\n");								//Leave 2 lines by using println and \n
-					break;													//break;
-				case "2":													//if menu=2
+					int i=0;							//create a variable int type named "i" and assign it to 0 for count and display databases sequence.
+					for(String DB : dbs) {						//create for loop to display database name.
+						i++;							//i=i+1
+						System.out.println(i+". "+DB);				//print databases sequence and database name.
+					}System.out.println("\n");					//Leave 2 lines by using println and \n
+					break;								//break;
+				case "2":								//if menu=2
 					System.out.println("\n********* Showing All of Document of the Customer Collection ********");//print interface.
 					DisplayDocument();
-					break;													//break;
+					break;								//break;
 				case "3":
 					System.out.println("\n************ Adding the Person information ************************");
 					BasicDBObject document = new BasicDBObject();
